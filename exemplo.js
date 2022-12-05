01041ccc88b02e0d0a0c0bae8727ea63d1b14cd0
Rodrigo Gregori <rodrigo.gregori@catolicasc.org.br>
2022-12-05 20:21:50 -0300

    Exemplos com selectbox

--- exemplo.js
+++ exemplo.js
@@ -1,4 +1,37 @@
-function sum() {
+let op = function() {
+    alert("Escolha uma opção antes");
+} 
+
+function sum(num1, num2) {
+    return num1 + num2;
+}
+
+function sub(num1, num2) {
+    return num1 - num2;
+}
+
+
+function operation() {
+    const answer = document.getElementById('answer');
+    const select = document.getElementById('operation');
+
+    const selectedOperation = select.selectedIndex;
+
+    switch (selectedOperation) {
+        case 1:
+            answer.innerText = 'Adição';
+            op = sum;
+            break;
+        case 2:
+            answer.innerText = 'Subtração';
+            op = sub;
+            break;
+        default:
+            break;
+    }
+}
+
+function calculate() {
     // referências aos elementos
     const num1Input = document.getElementById('num1');
     const num2Input = document.getElementById('num2');
@@ -7,7 +40,7 @@ function sum() {
     // valores dos inputs
     const num1 = parseInt(num1Input.value);
     const num2 = parseInt(num2Input.value);
-    let numSum = num1 + num2;
-    // insiro o texto da resposta
-    answer.innerHTML = numSum;
+    const resp = op(num1, num2);
+
+    answer.innerText = resp;
 }
